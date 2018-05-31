2aae7f9335a7d692846f9e6640f7271f7b915a7a
Bryan <bryan.pilco@gmail.com>
2018-05-31 10:08:19 -0500

    Agregando primer ejercicio

---
+++ Arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arreglos;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Arreglos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[] c = new int[4];
+        c[0] = 10;
+        for (int i = 0; i < c.length; i++) {
+            
+            System.out.println(c[i]);
+            
+        }
+        
+    }
+    
+}
